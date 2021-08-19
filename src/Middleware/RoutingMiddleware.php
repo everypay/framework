71fc321ef9566963f9fbd78227f45a689c92c8f8
kmavrofridis <kmavrofridis@everypay.gr>
2021-08-19 14:27:02 +0300

    Added routing middleware

---
+++ src/Middleware/RoutingMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Everypay\Framework\Middleware;
+
+use Everypay\Framework\Contract\Routing\RouterInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RoutingMiddleware implements MiddlewareInterface
+{
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $this->router->dispatch($request);
+
+        $request = $request->withAttribute('_action', $this->router->getAction());
+
+        foreach ($this->router->getArguments() as $name => $value) {
+            $request = $request->withAttribute($name, $value);
+        }
+
+        return $handler->handle($request);
+    }
+}
