07ba9273567c77cf9507e11101db0e2ff2890d3f
kmavrofridis <kmavrofridis@everypay.gr>
2021-08-19 15:34:48 +0300

    Added actionResolver Middleware

---
+++ src/Middleware/ActionResolverMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Everypay\Framework\Middleware;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ActionResolverMiddleware implements MiddlewareInterface
+{
+    private ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $action = $request->getAttribute('_action');
+        if (is_string($action)) {
+            $action = $this->container->get($action);
+        }
+
+        return call_user_func_array($action, [$request]);
+    }
+}
